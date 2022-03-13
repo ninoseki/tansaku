b29e4db19381a4f7d20b433d4c0e486407c04708
Manabu Niseki <manabu.niseki@gmail.com>
2022-03-13 15:05:11 +0900

    feat: add method & body options

--- lib/tansaku/cli.rb
+++ lib/tansaku/cli.rb
@@ -8,7 +8,9 @@ module Tansaku
   class CLI < Thor
     desc "crawl URL", "Crawl a given URL"
     method_option :additional_list, desc: "Path to the file which includes additional paths to crawl"
-    method_option :headers, type: :hash, default: {}, desc: "Headers to use"
+    method_option :headers, type: :hash, default: {}, desc: "HTTP headers to use"
+    method_option :method, type: :string, default: "HEAD", desc: "HTTP method to use"
+    method_option :body, type: :string, required: false, default: nil, desc: "HTTP reqeust body to use"
     method_option :max_concurrent_requests, type: :numeric, desc: "Number of concurrent requests to use"
     method_option :type, desc: "Type of a list to crawl (admin, backup, database, etc, log or all)", default: "all"
     def crawl(url)
